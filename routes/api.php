50904923cb590890480e26540b78402159d1b825
Corfaya <lauracorrado0696@gmail.com>
2024-10-14 15:22:26 +0200

    [UPD]+ Milestone 2: test success

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
+// Thunder Client returns data requested
