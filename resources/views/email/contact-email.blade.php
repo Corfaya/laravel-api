cc9804390cd8e64547ec2b293d86dcef79dbb11a
Corfaya <lauracorrado0696@gmail.com>
2024-10-17 17:10:18 +0200

    [UPD] contact-email view updated with a realistic message

--- resources/views/email/contact-email.blade.php
+++ resources/views/email/contact-email.blade.php
@@ -1,1 +1,9 @@
-<h1>Ciao</h1>
+<h1 style="text-align: center">Hello Admin!</h1>
+<p>You received a message from:</p>
+<ul>
+    <li>{{$lead->surname }}, {{$lead->name}}</li>
+    <li><strong>Phone number</strong>: {{ $lead->phone }}</li>
+    <li><strong>@</strong>: {{ $lead->email_address }}</li>
+</ul>
+<p><strong style="color:brown">The message</strong>:</p>
+<p>{{$lead->content}}</p>
