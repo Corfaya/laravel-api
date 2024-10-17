552481a3ffb3c51a7196e25427f6349973a2c077
Corfaya <lauracorrado0696@gmail.com>
2024-10-17 15:58:22 +0200

    [UPD] route set in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 
 /*
@@ -24,4 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
 Route::get('/projects/{slug}', [ProjectController::class, 'details'])->name('details');
 Route::get('/latest_projects', [ProjectController::class, 'latest'])->name('latest_projects');
-// Thunder Client returns data requested
+// for contacts:
+Route::post('/contacts', [LeadController::class, 'store'])->name('email_sending');
